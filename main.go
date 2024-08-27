4a97c59bb0bc9a245d8ce256391e87cf97a029ef
곽민우 <g.kmwdev@gmail.com>
2024-08-27 18:16:55 +0900

    Feat: 간단한 GET API 추가

--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-  "fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 //TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
 
 func main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-  // to see how GoLand suggests fixing it.
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
+	r := gin.Default()
 
-  for i := 1; i <= 5; i++ {
-	//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session, 
-	// right-click your code in the editor and select the <b>Debug</b> option. 
-	fmt.Println("i =", 100/i)
-  }
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "잘해써~")
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	r.Run(":5173")
 }
 
 //TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
